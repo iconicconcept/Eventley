c5358d232d49f522668302a702bc08d5f751a2f5
Mubaraq Allamalyekeen <mubaraqadeniyi159@gmail.com>
2025-06-17 00:46:13 +0100

    backend error commit

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -34,7 +34,7 @@ app.use("/api/events", eventRoutes)
 app.use("/api/user", userRouter)
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.use(express.static(path.join(__dirname, "../Frontend/dist")));
     app.get("*",(req, res)=>{
         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
     });
